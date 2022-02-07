a697a5a2c06e42b35c7b19c3e98b7779a2e35274
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2022-02-07 18:33:48 +0200

    created form for calculation

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+const confirmPasswordEl = document.querySelector('#confirm-password');
+
+const form = document.getElementById('signup');
+const perfomanceWeight1 = form.elements['perfomanceWeight-1'];
+const  perfomanceScore1 = form.elements['perfomanceScore-1'];
+
+
+
+
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+     console.log(perfomanceWeight1.value, perfomanceScore1.value)
+});
