1bcd81e319ea0e895ad75ee3084c284617e3186f
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2022-02-09 13:18:42 +0200

    finished form

--- main.js
+++ main.js
@@ -1,58 +1,40 @@
 
 
 const form = document.getElementById('signup');
-
-
-
-
-
-
-
-
-
-
+const allScore = document.getElementById("final");
 
 
 let finalScore = 0;
 
-let perfomance = document.getElementById("perfomance")
+let perfomanceWeightInputs = document.getElementsByClassName("perfomanceWeight");
+let perfomanceScoreInputs = document.getElementsByClassName("perfomanceScore");
 
+let easyOfUseWeightInputs = document.getElementsByClassName("EasyOfUseWeight");
+let easyOfUseScoreInputs = document.getElementsByClassName("EasyOfUseScore");
 
-let  perfomanceWeightInputs = perfomance.getElementsByClassName("perfomanceWeight");
-let  perfomanceScoreInputs = perfomance.getElementsByClassName("perfomanceScore");
+let complianceWeightInputs = document.getElementsByClassName("ComplianceWeight");
+let complianceScoreInputs = document.getElementsByClassName("ComplianceScore");
 
-let  easyOfUseWeightInputs = document.getElementsByClassName("EasyOfUseWeight");
-let  easyOfUseScoreInputs = document.getElementsByClassName("EasyOfUseScore");
-
-let  complianceWeightInputs = document.getElementsByClassName("ComplianceWeight");
-let  complianceScoreInputs = document.getElementsByClassName("ComplianceScore");
-
-
-let some =(weights, scores) =>{
+let some = (weights, scores) => {
 
   let smallSome = (arr) => {
     let newArr = [];
-    for(let i=0; i<arr.length; i++){
-     newArr.push(+arr[i].value)
+    for (let i = 0; i < arr.length; i++) {
+      newArr.push(+arr[i].value)
     }
-   return newArr
+    return newArr
   };
-
-  
 
   let weightsValues = smallSome(weights);
   let scoreValues = smallSome(scores);
   let result = [weightsValues, scoreValues]
- 
+
   return result
 };
 
-
 let calculate = (arr) => {
-  
-  
-  let sum = arr[0].reduce((prev, curr) => +prev + +curr);
 
+  let sum = arr[0].reduce((prev, curr) => +prev + +curr);
 
   let weightPercentage = arr[0].map((item) => (item / sum * 100));
   let weightedValueSum = 0;
@@ -66,40 +48,29 @@ let calculate = (arr) => {
   return result;
 }
 
-
 function makeCounter(arr) {
-  console.log(arr)
-  return function() {
-    for(let i = 0; i < arr.length; i++){
-      if(!isNaN(arr[i])){
-       finalScore += arr[arr.length - 1]
+  // console.log(arr)
+  return function () {
+    for (let i = 0; i < arr.length; i++) {
+      if (!isNaN(arr[i])) {
+        finalScore += arr[arr.length - 1]
       }
     }
   };
 }
 
-
-
-
-
-
-
-
 form.addEventListener('submit', function (e) {
 
-  
-  
   e.preventDefault();
-  
-  
-  console.log(calculate(some(perfomanceWeightInputs, perfomanceScoreInputs)));
-  
-  console.log(calculate(some(easyOfUseWeightInputs, easyOfUseScoreInputs)));
-  console.log(calculate(some(complianceWeightInputs, complianceScoreInputs)));
-  
 
-  // calculateOverral(finalScore)
-  
-  console.log((finalScore / 2 / 4 / 5 * 100).toFixed() + "%")
- 
+  let perfomanceScore = calculate(some(perfomanceWeightInputs, perfomanceScoreInputs))[0] + "%";
+  document.getElementById("perfomanceFinalScore").innerHTML = perfomanceScore;
+
+  let easyScore = calculate(some(easyOfUseWeightInputs, easyOfUseScoreInputs))[0] + "%";
+  document.getElementById("easyFinalScore").innerHTML = easyScore;
+
+  let complianceScore = calculate(some(complianceWeightInputs, complianceScoreInputs))[0] + "%";
+  document.getElementById("complianceFinalScore").innerHTML = complianceScore;
+
+  document.getElementById("final").innerHTML = (finalScore / 2 / 4 / 5 * 100).toFixed() + "%";
 });
